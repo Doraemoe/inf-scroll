1001269553c4904a422a3705b024ed6ef04dd2d9
Yifan Jin <yijin@expedia.com>
2023-11-10 10:38:28 +1000

    Initial Commit

---
+++ InfLoadTest/InfLoadTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfLoadTestApp.swift
+//  InfLoadTest
+//
+//  Created by Yifan Jin on 10/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct InfLoadTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
